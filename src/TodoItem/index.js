dda25fa24207b8e0b182ace3d08c2cb523f6224c
RicardoAguilera92 <ricardo.aguilera92@icloud.com>
2023-07-01 19:30:32 -0600

    Creacion de aplicacion react

---
+++ src/TodoItem/index.js
@@ -0,0 +1,18 @@
+import { CompleteIcon } from "../TodoIcon/completeIcon";
+import { DeleteIcon } from "../TodoIcon/deleteIcon";
+import "./TodoItem.css";
+
+function TodoItem(props) {
+  return (
+    <li className="TodoItem">
+      <CompleteIcon completed={props.completed} onComplete={props.onComplete} />
+
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
+      </p>
+      <DeleteIcon onDelete={props.onDelete} />
+    </li>
+  );
+}
+
+export { TodoItem };
